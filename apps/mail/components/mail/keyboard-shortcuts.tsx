40e7e1eb94d71b35b8d6bbe50d7a8a288966f5cd
Ahmet Kilinc <akx9@icloud.com>
2025-02-28 17:58:51 +0000

    add hot key for zero

--- apps/mail/components/mail/keyboard-shortcuts.tsx
+++ apps/mail/components/mail/keyboard-shortcuts.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useAISidebar } from "@/components/ui/ai-sidebar";
 import { navigationConfig } from "@/config/navigation";
 import { useHotKey } from "@/hooks/use-hot-key";
 import { useRouter } from "next/navigation";
 
 export const KeyboardShortcuts = () => {
   const router = useRouter();
+  const { toggleOpen } = useAISidebar();
   if (navigationConfig?.mail?.sections) {
     const items = navigationConfig.mail.sections[0]?.items || [];
     items.map((item, index) => {
@@ -16,6 +18,14 @@ export const KeyboardShortcuts = () => {
       }
     });
   }
+
+  useHotKey("Meta+0", () => {
+    toggleOpen();
+  });
+
+  useHotKey("Control+0", () => {
+    toggleOpen();
+  });
 
   return null;
 };
